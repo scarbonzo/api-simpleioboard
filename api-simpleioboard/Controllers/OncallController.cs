ee7e9a5a11cc275892484c53613f9b6df68e3564
Rich Eodice <rich@eodice.com>
2023-02-14 10:58:38 -0500

    V1 done

---
+++ api-simpleioboard/Controllers/OncallController.cs
@@ -0,0 +1,54 @@
+﻿using api_simpleioboard.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace api_simpleioboard.Controllers
+{
+    [ApiController]
+    public class OncallController : ControllerBase
+    {
+        // GET: OncallController
+        [Route("api/oncall")]
+        [HttpGet]
+        public ActionResult GetTeamOncall(int year, int week, string team)
+        {
+            var dbClient = new MongoClient(configuration.dbconnection);
+            var database = dbClient.GetDatabase(configuration.dbname);
+            var colOncall = database.GetCollection<Oncall>(configuration.oncallscollection);
+
+            var result = colOncall
+                .AsQueryable()
+                .Where(o => o.Year == year)
+                .Where(o => o.Week == week)
+                .Where(o => o.Team == team)
+                .ToList();
+
+            return Ok(result);
+        }
+
+        // GET: api/oncall/create
+        [Route("api/oncall/create")]
+        [HttpGet]
+        public ActionResult CreateOncall(int year, int week, string team, string primary, string backup)
+        {
+            var dbClient = new MongoClient(configuration.dbconnection);
+            var database = dbClient.GetDatabase(configuration.dbname);
+            var colOncall = database.GetCollection<Oncall>(configuration.oncallscollection);
+
+            var status = new Oncall
+            {
+                Id = Guid.NewGuid(),
+                Year = year,
+                Week = week,
+                Team = team,
+                Primary = primary,
+                Backup = backup
+            };
+
+            colOncall.InsertOne(status);
+
+            return Ok(status);
+        }
+    }
+}
---
+++ api-simpleioboard/Models/Oncall.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver.Linq;
+using System.Globalization;
+
+namespace api_simpleioboard.Models
+{
+    public class Oncall
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Year { get; set; } = 0;
+        public int Week { get; set; } = 0;
+        public string Team { get; set; } = "";
+        public string Primary { get; set; } = "";
+        public string Backup { get; set; } = "";
+    }
+}
